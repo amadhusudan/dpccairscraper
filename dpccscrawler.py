bb51afa384197e940fa465b61126793be84761eb
A. Madhusudan <amadhusudan@debian.mad>
2016-02-17 20:52:03 +0530

    Initial Commit of the DPCC Data Scraper
    Signed-off-by: A. Madhusudan <amadhusudan@debian.mad>

---
+++ dpccscrawler.py
@@ -0,0 +1,83 @@
+from __future__ import unicode_literals
+ 
+from google.appengine.ext import db
+from datetime import datetime
+import urllib2
+from lxml import html
+
+import pdb
+
+DPCC_URL_AREA_ANAND_VIHAR = 'http://www.dpccairdata.com/dpccairdata/display/avView15MinData.php'
+DPCC_URL_AREA_MANDIR_MARG = 'http://www.dpccairdata.com/dpccairdata/display/mmView15MinData.php'
+DPCC_URL_AREA_PUNJABI_BAGH = 'http://www.dpccairdata.com/dpccairdata/display/pbView15MinData.php'
+DPCC_URL_AREA_RK_PURAM = 'http://www.dpccairdata.com/dpccairdata/display/rkPuramView15MinData.php'
+DPCC_URL_AREA_IGI_AIRPORT = 'http://www.dpccairdata.com/dpccairdata/display/airpoView15MinData.php'
+DPCC_URL_AREA_CIVIL_LINES = 'http://www.dpccairdata.com/dpccairdata/display/civilLinesView15MinData.php'
+
+DPCC_URL_ALL_AREAS = {
+    'ANAND VIHAR' : DPCC_URL_AREA_ANAND_VIHAR,
+    'MANDIR MARG' : DPCC_URL_AREA_MANDIR_MARG,
+    'PUNJABI BAGH': DPCC_URL_AREA_PUNJABI_BAGH,
+    'RK PURAM' : DPCC_URL_AREA_RK_PURAM,
+    'CIVIL LINES' : DPCC_URL_AREA_CIVIL_LINES
+    }
+
+def get_datetime(dt, tm):
+    dtmstring = "%s::%s" %(dt, tm)
+    dtm = datetime.strptime(dtmstring, "%A, %B %d, %Y::%H:%M:%S")
+    return dtm
+
+def unicode_escape(v):
+    return v.encode('unicode-escape')
+
+class DPCCItem(db.Model):
+    area = db.StringProperty(required=True)
+    parameter = db.StringProperty(required=True)
+    date = db.DateTimeProperty(required=True)
+    value = db.StringProperty(required=True)
+    standard = db.StringProperty(required=False)
+
+    def __str__(self):
+        return "Area: %s::Date: %s, Parameter: %s, Value: %s, Standard: %s" %(self.area, str(self.date), 
+                str(self.parameter), unicode_escape(self.value), unicode_escape(self.standard))
+
+class DPCCAirScrawler:
+    @staticmethod
+    def scrape():
+        for a, area_url in DPCC_URL_ALL_AREAS.iteritems():
+            try:
+                response = urllib2.urlopen(area_url)
+                raw_html = response.read()
+                dom = html.fromstring(raw_html)
+                pollution_data = dom.xpath("//tr[@class='tdcolor1']|//tr[@class='tdcolor2']")
+                for pd in pollution_data:
+                    pm = pd.xpath('./td[1]/text()')[0].strip()
+                    dt = pd.xpath('./td[2]/text()')[0].strip()
+                    tm = pd.xpath('./td[3]/text()')[0].strip()
+                    vl = pd.xpath('./td[4]/text()')[0].strip()
+                    ut = pd.xpath('./td[4]/text()')[0].strip()
+                    st = pd.xpath('./td[5]/text()')[0].strip()
+
+                    # Check if value has a <sup> (superscript) as well and get that
+                    sup = pd.xpath('./td[4]/sup/text()')
+                    
+                    if sup:
+                        sup = sup[0].strip()
+                        vl = vl + sup
+                        ut = ut + sup
+                        st = st + sup
+
+                    print "ar: %s, pm: %s, dt: %s, tm: %s, vl: %s, st: %s" % (a, str(pm), str(dt), str(tm), 
+                                            unicode_escape(vl), unicode_escape(st))
+
+                    item = DPCCItem(area=a, parameter=str(pm), value=vl, date=get_datetime(dt, tm))
+                    item.standard = st
+                    item.put()
+            except urllib2.URLError as e:
+                print "URL Open Status Code Exception: %s" % str(e)
+
+        retval = ''
+        for item in DPCCItem.all():
+            retval = retval + "\n" + str(item)
+
+        return retval
---
+++ main.py
@@ -0,0 +1,31 @@
+"""`main` is the top level module for your Flask application."""
+
+# Import the Flask Framework
+from flask import Flask
+from dpccscrawler import DPCCAirScrawler
+
+app = Flask(__name__)
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+@app.route('/8dqUUbjDbujeXgEyPQBhjevQTjHJSHd748YqvphFdkQGTgP6QUh4tXcMUuWbGT2R7XDJqDjb4EhGjG7pVrFq3wQV4F4qmWTGaTrp')
+def scrape():
+    return DPCCAirScrawler.scrape()
+    return ''
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
